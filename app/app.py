a5f667f9223ba15f5e013e05506202a1334027bb
vibhakar246 <102617175+vibhakar246@users.noreply.github.com>
2025-12-18 12:51:57 +0530

    Fix Flask app syntax and expose app on port 5000

--- app/app.py
+++ app/app.py
@@ -1,1 +1,10 @@
-return "Auto Deployment Working 🚀🚀"
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Auto Deployment Working 🚀🚀"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
