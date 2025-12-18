6f4e179facab9f673c5fc1ee32f0b7f65ca8d679
vibhakar246 <102617175+vibhakar246@users.noreply.github.com>
2025-12-18 10:18:36 +0530

    Commit directly to main branch

--- app/app.py
+++ app/app.py
@@ -1,10 +1,1 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Flask + MySQL CI/CD Project by Vibhakar246 🚀"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+return "Auto Deployment Working 🚀🚀"
